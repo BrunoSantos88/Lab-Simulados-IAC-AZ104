6b20da4dfbfa8339d1dc793b46daaa9643f44afb
Aperture-IT-Solutions <brunosantos@v3hfv.onmicrosoft.com>
2023-10-04 22:35:24 -0300

    azureaks

--- aks/aks.tf
+++ aks/aks.tf
@@ -11,17 +11,26 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
     name       = "default"
-    node_count = 3
+    node_count = 1
     vm_size    = "Standard_D2_v2"
   }
-}
 
-resource "azurerm_kubernetes_cluster_node_pool" "example" {
-  name                  = "internal"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
-  vm_size               = "Standard_DS2_v2"
-  node_count            = 3
+  identity {
+    type = "SystemAssigned"
+  }
+
   tags = {
     Environment = "Production"
   }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
 }
